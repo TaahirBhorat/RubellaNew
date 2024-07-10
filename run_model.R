38fb7ef5f25906d760db16bbd3ba35dde1120b09
= <=>
2024-07-10 10:26:15 +0200

    sdf

--- run_model.R
+++ run_model.R
@@ -68,65 +68,82 @@ rubella_data  = as.data.frame(mo_baseline$moRaw)
 library(ggplot2)
 library(dplyr)
 library(gridExtra)
-
 # Extract the 'time' column (assumed to be the first column in your data)
 time_column <- rubella_data[, 1]
 
-# Extract columns for each compartment using their respective column indices
 M_columns <- rubella_data[, seq(2, ncol(rubella_data), by=5)]
 S_columns <- rubella_data[, seq(3, ncol(rubella_data), by=5)]
 I_columns <- rubella_data[, seq(4, ncol(rubella_data), by=5)]
 R_columns <- rubella_data[, seq(5, ncol(rubella_data), by=5)]
 V_columns <- rubella_data[, seq(6, ncol(rubella_data), by=5)]
 
-# Sum all compartments across all ages for each time point
-total_M <- rowSums(M_columns)
-total_S <- rowSums(S_columns)
-total_I <- rowSums(I_columns)
-total_R <- rowSums(R_columns)
-total_V <- rowSums(V_columns)
+age_bins <- list(
+  "0-4 years" = 1:48,       # Monthly bins from 0-47 months (0-4 years)
+  "5-59 years" = 49:104,    # Yearly bins from 5-59 years
+  "60-90+ years" = 105:ncol(I_columns) # Decade bins from 60-90+ years
+)
 
-# Create data frames for plotting
-plot_data_M <- data.frame(time = time_column, total = total_M, compartment = "M")
-plot_data_S <- data.frame(time = time_column, total = total_S, compartment = "S")
-plot_data_I <- data.frame(time = time_column, total = total_I, compartment = "I")
-plot_data_R <- data.frame(time = time_column, total = total_R, compartment = "R")
-plot_data_V <- data.frame(time = time_column, total = total_V, compartment = "V")
+sum_age_bins <- function(data, bins) {
+  sapply(bins, function(cols) rowSums(data[, cols, drop = FALSE]))
+}
+
+sum_age_bins <- function(data, bins) {
+  result <- matrix(nrow = nrow(data), ncol = length(bins))
+  colnames(result) <- names(bins)
+  for (i in seq_along(bins)) {
+    result[, i] <- rowSums(data[, bins[[i]], drop = FALSE])
+  }
+  result
+}
+
+M_bins <- sum_age_bins(M_columns, age_bins)
+S_bins <- sum_age_bins(S_columns, age_bins)
+I_bins <- sum_age_bins(I_columns, age_bins)
+R_bins <- sum_age_bins(R_columns, age_bins)
+V_bins <- sum_age_bins(V_columns, age_bins)
+
+create_plot_data <- function(time, bins, compartment_name) {
+  do.call(rbind, lapply(seq_along(bins), function(i) {
+    data.frame(time = time, total = bins[, i], age_band = colnames(bins)[i], compartment = compartment_name)
+  }))
+}
+
+plot_data_M <- create_plot_data(time_column, M_bins, "M")
+plot_data_S <- create_plot_data(time_column, S_bins, "S")
+plot_data_I <- create_plot_data(time_column, I_bins, "I")
+plot_data_R <- create_plot_data(time_column, R_bins, "R")
+plot_data_V <- create_plot_data(time_column, V_bins, "V")
 
 # Combine data frames into one
 plot_data <- rbind(plot_data_M, plot_data_S, plot_data_I, plot_data_R, plot_data_V)
 
-# Define a plotting function
+
 plot_compartment <- function(data, title) {
-  ggplot(data, aes(x = time, y = total)) +
+  ggplot(data, aes(x = time, y = total, color = age_band)) +
     geom_line() +
     geom_point() +
     labs(title = title, x = "Time", y = "Count") +
-    theme_minimal()
+    theme_minimal() +
+    theme(legend.position = "bottom")
 }
 
-# Generate individual plots for each compartment
-plot_M <- plot_compartment(plot_data_M, "Total M Compartment Over Time")
-plot_S <- plot_compartment(plot_data_S, "Total S Compartment Over Time")
-plot_I <- plot_compartment(plot_data_I, "Total I Compartment Over Time")
-plot_R <- plot_compartment(plot_data_R, "Total R Compartment Over Time")
-plot_V <- plot_compartment(plot_data_V, "Total V Compartment Over Time")
+plot_M <- plot_compartment(plot_data[plot_data$compartment == "M", ], "M Compartment Over Time by Age Band")
+plot_S <- plot_compartment(plot_data[plot_data$compartment == "S", ], "S Compartment Over Time by Age Band")
+plot_I <- plot_compartment(plot_data[plot_data$compartment == "I", ], "I Compartment Over Time by Age Band")
+plot_R <- plot_compartment(plot_data[plot_data$compartment == "R", ], "R Compartment Over Time by Age Band")
+plot_V <- plot_compartment(plot_data[plot_data$compartment == "V", ], "V Compartment Over Time by Age Band")
 
-# Arrange the plots in a grid
 grid.arrange(plot_M, plot_S, plot_I, plot_R, plot_V, ncol = 1)
-
 
 
 ###### AGE: 
 
 
-# Extract the 'time' column (assumed to be the first column in your data)
 time_column <- rubella_data[, 1]
 
 # Extract the 'I' compartments
 I_columns <- rubella_data[, seq(4, ncol(rubella_data), by=5)]
 
-# Define the age bins based on your specified age categories
 age_bins <- c(
   seq(0.5/12, 47.5/12, by=1/12), # Monthly bins from 0-47 months (0-4 years)
   seq(4.5, 59.5, by=1),          # Yearly bins from 5-59 years
